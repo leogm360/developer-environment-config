2b900d90f17e0e8e085d9925f544af9a34ece0b3
leogm360 <leogm360@gmail.com>
2023-03-12 17:14:11 -0300

    🐛 fix(scripts): postgres installation

--- src/scripts/8-install-postgres.sh
+++ src/scripts/8-install-postgres.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+CWD=$PWD
 POSTGRES_USER="$USER"
 POSTGRES_USER_PASSWORD='123456789'
 POSTGRES_DATABASE="$USER"
@@ -7,28 +8,16 @@ POSTGRES_DATABASE="$USER"
 echo -e "$BOLD_LIGHT_BLUE\nSTART POSTGRESQL INSTALL...\n$NO_COLOR"
 
 # add postgresql repository to apt list
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # download postgresql keys to apt keyring
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-# go to apt list dir
-cd /etc/apt/sources.list.d/ || exit 1
-
-# remove uncompatible postgresql keyring
-sudo rm -f pgdg.list
-
-# recreate postgresql keyring
-sudo touch pgdg.list
-
-# dump compatible keyring to postgresql keyring
-echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee -a pgdg.list
-
 # update apt index and install postgresql
 sudo apt update && sudo apt install postgresql -y
 
-# go to postgres dir
-cd ~postgres/ || exit 1
+# got to dir where postgres user has permissions
+cd ~postgres/ || throw 5
 
 # directs sql query to postgresql cli
 sudo -u postgres psql <<query
@@ -37,5 +26,8 @@ sudo -u postgres psql <<query
   CREATE DATABASE $POSTGRES_DATABASE;
   ALTER DATABASE $POSTGRES_DATABASE OWNER TO $POSTGRES_USER;
 query
+
+# go back to scripts directory
+cd "$CWD" || throw 5
 
 echo -e "$BOLD_LIGHT_BLUE\nEND POSTGRESQL INSTALL...\n$NO_COLOR"
