3f5ac7317a72d3faa623cd1dba7780f708739446
leogm360 <leogm360@gmail.com>
2023-03-23 11:00:33 -0300

    feat(pre-install): add synaptic package manager to pre install

--- src/scripts/1-install-pre.sh
+++ src/scripts/1-install-pre.sh
@@ -9,6 +9,6 @@ sudo apt update && sudo apt upgrade -y
 sudo add-apt-repository multiverse
 
 # install pre dependencies and video card drivers
-sudo apt install curl wget ubuntu-restricted-extras nvidia-driver-525 -y
+sudo apt install curl wget synaptic ubuntu-restricted-extras nvidia-driver-525 -y
 
 echo -e "$BOLD_LIGHT_BLUE\nEND PRE INSTALL...\n$NO_COLOR"
