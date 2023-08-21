8f45c2859955a9834ee34d1717455423fd76fe2a
leogm360 <leogm360@gmail.com>
2023-08-21 10:52:40 -0300

    fix: typo

--- src/scripts/1-install-pre.sh
+++ src/scripts/1-install-pre.sh
@@ -2,13 +2,13 @@
 
 echo -e "$BOLD_LIGHT_BLUE\nSTART PRE INSTALL...\n$NO_COLOR"
 
-# first update apt index and upgrade all dependencies
+# first update apt index and upgrade all packages
 sudo apt update && sudo apt upgrade -y
 
-# add multiverse ubuntu repository ro apt list
+# add multiverse ubuntu repository to apt list
 sudo add-apt-repository multiverse
 
-# install pre dependencies and video card drivers
+# install pre packages and video card drivers
 sudo apt install curl wget synaptic ubuntu-restricted-extras nvidia-driver-525 -y
 
 echo -e "$BOLD_LIGHT_BLUE\nEND PRE INSTALL...\n$NO_COLOR"
