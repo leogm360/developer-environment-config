ae83c91628e9cbf818ae29c190b2a24cc5d1242f
leogm360 <leogm360@gmail.com>
2023-08-25 10:51:36 -0300

    feat: add install script for kubectl

---
+++ src/scripts/20-install-kubectl.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Get kubectl repository sign key and add to local keyring
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+# Add kubectl repository to apt index
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update apt index and install kubectl from private repository
+sudo apt update && sudo apt install kubectl -y
